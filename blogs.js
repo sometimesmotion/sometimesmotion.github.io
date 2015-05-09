41b3d5a8676742822c85779cf8ffcb0159e8e00c
Tim Miller <twm2@humboldt.edu>
2015-05-08 22:41:33 -0700

    Create blogs.js

---
+++ blogs.js
@@ -0,0 +1,13 @@
+var blogs = [
+{"title":"Web Literacy and #CritLib",
+     "date":"05/05/15",
+     "URL":"http://sometimesmotion.net/critlib-webliteracy.html"},
+  
+    {"title":"Second Blog", "date":"05/15/15",
+     "URL":"http://sometimesmotion.net/critlib-webliteracy.html"},
+  
+    {"title":"Third Blog", "date":"05/21/25",
+     "URL":"http://sometimesmotion.net/critlib-webliteracy.html"}
+];
+//$('#archive ul').prepend("hello world");
+$('#archive ul').append('<li>'+blogs[0].date+' - <a href="'+blogs[0].URL+'" target="_blank">'+blogs[0].title+'</a>');
