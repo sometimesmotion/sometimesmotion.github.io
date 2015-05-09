05af1784664c837a2f04312c0eb3a3eca6741ec9
Tim Miller <twm2@humboldt.edu>
2015-05-09 16:56:40 -0700

    Create posts.js

---
+++ posts.js
@@ -0,0 +1,27 @@
+postsData([
+{
+"title": "HTML Tutorial",
+"url": "http://www.w3schools.com/html/default.asp",
+"date":"5/1/15"
+},
+{
+"title": "CSS Tutorial",
+"url": "http://www.w3schools.com/css/default.asp",
+"date":"5/2/15"
+},
+{
+"title": "JavaScript Tutorial",
+"url": "http://www.w3schools.com/js/default.asp",
+"date":"5/3/15"
+},
+{
+"title": "jQuery Tutorial",
+"url": "http://www.w3schools.com/jquery/default.asp",
+"date":"5/4/15"
+},
+{
+"title": "JSON Tutorial",
+"url": "http://www.w3schools.com/json/default.asp",
+"date":"5/5/15"
+}
+])
