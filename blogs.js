6c1fa745fd6bf63468c51fa037b8b26b60b72994
Tim Miller <twm2@humboldt.edu>
2015-05-09 17:53:08 -0700

    Update blogs.js

--- blogs.js
+++ blogs.js
@@ -38,3 +38,24 @@ function ssInsert() {
   $('head').append("<link href='https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.4/flatly/bootstrap.min.css' rel='stylesheet' type='text/css'>");
   $('head').append("<link rel='stylesheet' text='text/css' href='blogs.css'>");
 }//end ssInsert()
+
+function scrollToTop() {
+//Create button to scroll to top of page
+  $('body').append('<div class="toTopNav ion-ios-arrow-up"><span class="hidden">Back to Top</span></div>');
+  $('.toTopNav').css('cursor', 'pointer');
+//Scroll to the top of page
+  $('.toTopNav').click(function(){
+    $('html, body').animate({scrollTop:0}, 'fast');
+    return false;
+  });
+//Make button appear only when scrolled below 100px
+  $(window).scroll(function(){
+      if ($(this).scrollTop() > 400) {
+	    $('.toTopNav').show();
+	  }
+	  if ($(this).scrollTop() < 400) {
+	    $('.toTopNav').hide();
+	  }
+  });
+
+}
