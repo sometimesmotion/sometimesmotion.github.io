3d07f2edd0686315f27c6215fac2b46fe80fc660
Tim Miller <twm2@humboldt.edu>
2015-05-15 11:02:04 -0700

    Create otherLinks.js

---
+++ otherLinks.js
@@ -0,0 +1,22 @@
+otherData([
+{
+"text": "Projects",
+"url": "http://jsbin.com/cihibo/",
+"title":"Projects, Gists, &amp; Snippets"
+},
+{
+"text": "Bike Rides",
+"url": "http://sometimesmotion.net/rideMapper.html",
+"title":"Rides map (leaflet.js)"
+},
+{
+"text": "Instruction",
+"url": "http://sometimesmotion.net/projects.html",
+"title":"Lesson Plans"
+},
+{
+"text": "Business Card",
+"url": "#",
+"title":"Tim Miller's Contact Info"
+}
+])
