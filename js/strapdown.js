f2e9f3f5268fc2356135c62ad2119a2d93311592
Tim Miller <twm2@humboldt.edu>
2015-05-15 11:24:50 -0700

    Update strapdown.js

--- js/strapdown.js
+++ js/strapdown.js
@@ -371,7 +371,7 @@ var PR=win['PR']={'createSimpleLexer':createSimpleLexer,'registerLangHandler':re
   theme = theme.toLowerCase();
 
   // Stylesheets
-  var linkEl = document.createElement('link');
+  /*var linkEl = document.createElement('link');
   linkEl.href = originBase + '/themes/'+theme+'.min.css';
   linkEl.rel = 'stylesheet';
   document.head.appendChild(linkEl);
@@ -385,7 +385,7 @@ var PR=win['PR']={'createSimpleLexer':createSimpleLexer,'registerLangHandler':re
   linkEl.href = originBase + '/themes/bootstrap-responsive.min.css';
   linkEl.rel = 'stylesheet';
   document.head.appendChild(linkEl);
-
+*/
   //////////////////////////////////////////////////////////////////////
   //
   // <body> stuff
