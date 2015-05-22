0183e2bd54e2d061520d83a9bc13664fd72fb84a
Tim Miller <twm2@humboldt.edu>
2015-05-22 12:10:09 -0700

    Update dev-blogs.js

--- js/dev-blogs.js
+++ js/dev-blogs.js
@@ -9,3 +9,24 @@ function parseMd() {
 
  console.log('dev-blogs.js updated: 12:08'); 
 }
+
+function scrollToTop() {
+//Create button to scroll to top of page
+  $('body').append('<div class="toTopNav ion-ios-arrow-up"><span class="hidden">Back to Top</span></div>');
+  $('.toTopNav').css('cursor', 'pointer');
+//Scroll to the top of page
+  $('.toTopNav').click(function(){
+    $('html, body').animate({scrollTop:0}, 'fast');
+    return false;
+  });
+//Make button appear only when scrolled below 100px
+  $(window).scroll(function(){
+      if ($(this).scrollTop() > 400) {
+	    $('.toTopNav').show();
+	  }
+	  if ($(this).scrollTop() < 400) {
+	    $('.toTopNav').hide();
+	  }
+  });
+
+}
