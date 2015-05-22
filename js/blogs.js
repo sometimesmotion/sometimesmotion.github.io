caeaa67b3c9efe00f5a7a200e4399389e57cc1e9
Tim Miller <twm2@humboldt.edu>
2015-05-22 09:54:43 -0700

    Update blogs.js

--- js/blogs.js
+++ js/blogs.js
@@ -78,3 +78,15 @@ function otherData(links) {
   $('#otherList').append(output);
 }
 
+
+function parseMd() {
+
+$( ".marked" ).each(function( index ) {
+  var text = $(this).html();
+  var mdText = marked(text);
+  $(this).after('<div class="mdOutput"></div>');
+  $(this).next('.mdOutput').html(mdText);
+});
+  
+}
+
