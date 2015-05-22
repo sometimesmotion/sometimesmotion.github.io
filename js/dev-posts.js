ed52b4b09b4845722b058f808b9df6b56612abd3
Tim Miller <twm2@humboldt.edu>
2015-05-22 12:24:09 -0700

    Update dev-posts.js

--- js/dev-posts.js
+++ js/dev-posts.js
@@ -1,10 +1,10 @@
-var text = '{"name":"John Johnson","street":"Oslo West 16","phone":"555 1234567"}';
+var text = '{"title":"John Johnson","URL":"http://sometimesmotion.net/critlib-webliteracy.html","phone":"555 1234567"}';
 
 function postsData() {
 
 var obj = JSON.parse(text);
 
-$('#archiveList').append('<li>'+obj.name+'</li>');
+$('#archiveList').append('<li><a href="'+obj.URL+'">'+obj.title+'</a></li>');
 
   
 }
