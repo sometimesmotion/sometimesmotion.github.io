8e6f4143ce0c7f6c66afe9be13302a4a789e5c7b
Tim Miller <twm2@humboldt.edu>
2015-05-22 12:18:16 -0700

    Update dev-posts.js

--- js/dev-posts.js
+++ js/dev-posts.js
@@ -1,5 +1,10 @@
 var text = '{"name":"John Johnson","street":"Oslo West 16","phone":"555 1234567"}';
 
+function postsData() {
+
 var obj = JSON.parse(text);
 
 $('#display').append(obj.name);
+
+  
+}
