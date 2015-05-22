c6d5f17a7b9342b1197adbce0a519db415536107
Tim Miller <twm2@humboldt.edu>
2015-05-22 12:05:13 -0700

    Update dev-blogs.js

--- js/dev-blogs.js
+++ js/dev-blogs.js
@@ -6,5 +6,12 @@ function parseMd() {
     $(this).after('<div class="mdOutput"></div>');
     $(this).next('.mdOutput').html(mdText);
   });
-  
+ 
+ var d = new Date(); // for now
+d.getHours(); // => 9
+d.getMinutes(); // =>  30
+d.getSeconds(); // => 51
+var blogsTime = d.getHours()+':'+d.getMinutes()+":"+d.getSeconds();
+
+ console.log('dev-blogs.js updated: '+blogsTime); 
 }
