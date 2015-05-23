d59eac3272f1b4420c36e7c78cf0182b95c669f2
Tim Miller <twm2@humboldt.edu>
2015-05-22 19:23:59 -0700

    Update dev-posts.js

--- js/dev-posts.js
+++ js/dev-posts.js
@@ -22,3 +22,31 @@ function recentPosts() {
     $('#recent ul').append("<li><a href='"+blogs[i].url+"'>"+blogs[i].title+"</a></li>");
   }
 }
+
+
+for (var i = 0; i < blogs.length; i++) {
+  var url = blogs[i].url;
+
+  //find where the current page sits in list
+  var curURL = window.location.href; 
+  if(url === curURL){
+    var place = i;
+  }
+  var nextPlace = place + 1;
+  var postsLen = blogs.length;
+
+  //next
+  if (nextPlace < postsLen) {
+  var nextPost = blogs[nextPlace].url;
+    $('#next').show();
+  $('#next').attr('href',nextPost);
+  }
+
+  //prev
+  var prevPlace = place - 1;
+  if (prevPlace >= 0){
+  var prevPost = blogs[prevPlace].url;
+  $('#prev').show(); $('#prev').attr('href',prevPost);
+  }
+  
+}
