b69e9100552f6c61392e852fc4bd0d38ab3e36c1
Tim Miller <twm2@humboldt.edu>
2015-05-22 19:45:09 -0700

    Update dev-posts.js

--- js/dev-posts.js
+++ js/dev-posts.js
@@ -18,7 +18,8 @@ var blogs = [
     "category":"privacy"
   }
 ];
-
+$('#next').hide();
+$('#prev').hide();
 function postsData() {
   for (var i = 0; i < blogs.length; i++) {
     $('#archiveList').append('<li><a href="'+blogs[i].url+'">'+blogs[i].title+'</a></li>');
