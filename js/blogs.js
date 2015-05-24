6ff1766e32301f64980ca3357e7697ead1bbe3cd
Tim Miller <twm2@humboldt.edu>
2015-05-24 10:23:15 -0700

    Update blogs.js

--- js/blogs.js
+++ js/blogs.js
@@ -1,3 +1,24 @@
+function parseMd() {
+
+$( ".marked" ).each(function( index ) {
+  var text = $(this).text();
+  var mdText = marked(text);
+  //if md is w/in a <span> element, strip <p> tags
+  var tagType = $(this)[0].nodeName;
+  //console.log('type: '+tagType);
+  if (tagType === "SPAN") {
+    mdText = mdText.replace('<p>','');
+    mdText = mdText.replace('</p>','');
+    $(this).after('<span class="mdOutput"></span>');
+    $(this).next('.mdOutput').html(mdText);
+  } else {
+    $(this).after('<span class="mdOutput"></span>');
+    $(this).next('.mdOutput').html(mdText);
+  }
+});
+  
+}
+
 function scrollToTop() {
 //Create button to scroll to top of page
   $('body').append('<div class="toTopNav ion-ios-arrow-up"><span class="hidden">Back to Top</span></div>');
@@ -18,17 +39,3 @@ function scrollToTop() {
   });
 
 }
-
-
-function parseMd() {
-
-$( ".marked" ).each(function( index ) {
-  var text = $(this).html();
-  var mdText = marked(text);
-  $(this).after('<div class="mdOutput"></div>');
-  $(this).next('.mdOutput').html(mdText);
-});
-  
-}
-
-console.log('blogs.js was read');
