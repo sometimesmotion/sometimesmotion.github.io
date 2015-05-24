7510fde4a899a182425c5c5d605653cd4f57995c
Tim Miller <twm2@humboldt.edu>
2015-05-24 10:23:35 -0700

    Update links.js

--- js/links.js
+++ js/links.js
@@ -1,31 +1,29 @@
-function linksData(links) {
-  var output = "";
-  var i;
-  for(i = 0; i<links.length; i++) {
-    output += '<li><a href="'+links[i].url+'" target="_blank" title="' + links[i].title + '">'+links[i].text+'</a></li>';
+var links = [ 
+  {
+    "text": "Projects",
+    "url": "http://jsbin.com/cihibo/",
+    "title":"Projects, Gists, &amp; Snippets"
+  },
+  {
+    "text": "Bike Rides",
+    "url": "http://sometimesmotion.net/rideMapper.html",
+    "title":"Rides map (leaflet.js)"
+  },
+  {
+    "text": "Instruction",
+    "url": "http://sometimesmotion.net/projects.html",
+    "title":"Lesson Plans"
+  },
+  {
+    "text": "Business Card",
+    "url": "#",
+    "title":"Tim Miller's Contact Info"
   }
-  $('#otherList').append(output);
-}
+];
 
-linksData([
-{
-"text": "Projects",
-"url": "http://jsbin.com/cihibo/",
-"title":"Projects, Gists, &amp; Snippets"
-},
-{
-"text": "Bike Rides",
-"url": "http://sometimesmotion.net/rideMapper.html",
-"title":"Rides map (leaflet.js)"
-},
-{
-"text": "Instruction",
-"url": "http://sometimesmotion.net/projects.html",
-"title":"Lesson Plans"
-},
-{
-"text": "Business Card",
-"url": "#",
-"title":"Tim Miller's Contact Info"
+function linksData() {
+
+  for (var i = 0; i < links.length; i++) {
+    $('#linksList').append('<li><a href="'+links[i].url+'" target="_blank" title="' + links[i].title + '">'+links[i].text+'</a></li>');
+  } 
 }
-])
