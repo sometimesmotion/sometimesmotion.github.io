049ae3b5dd2410efa11533f26d0795de61f4fbf0
Tim Miller <twm2@humboldt.edu>
2015-05-25 09:46:45 -0700

    Update blogs.js

--- js/blogs.js
+++ js/blogs.js
@@ -40,3 +40,24 @@ function scrollToTop() {
 
 }
 
+function shareLinks(){
+	
+}
+  var curURL = window.location.href;
+  var msg = $('title').text();
+  //$('footer').append(msg);
+  var twitterURL = 'http://twitter.com/share?text='+msg+'&url='+curURL;
+console.log(twitterURL);
+  var gplusURL = 'https://plus.google.com/share?url='+curURL;
+  var tumblrURL = 'http://www.tumblr.com/share/link?url='+curURL+'&amp;name='+msg;
+  var facebookURL = 'https://www.facebook.com/sharer/sharer.php?u='+curURL+'&t='+msg;
+//Twitter
+  $('#shareLinks .dropdown-menu').append('<li><a href="'+twitterURL+'" target="_blank" class="twitterShare"><span class="hidden shareName">Twitter</span><span class="icon ion-social-twitter"></span></a></li>');
+//Facebook
+  $('#shareLinks .dropdown-menu').append('<li><a href="'+facebookURL+'" target="_blank" class="facebookShare"><span class="hidden shareName">Facebook</span><span class="icon ion-social-facebook"></span></a></li>');    
+//Tumblr
+  $('#shareLinks .dropdown-menu').append('<li><a href="'+tumblrURL+'" target="_blank" class="tumblrShare"><span class="hidden shareName">Tumblr</span><span class="icon ion-social-tumblr"></span></a></li>');    
+//Google+
+  $('#shareLinks .dropdown-menu').append('<li><a href="'+gplusURL+'" target="_blank" class="gplusShare"><span class="hidden shareName">Google +</span><span class="icon ion-social-googleplus-outline"></span></a></li>');    
+  
+}
